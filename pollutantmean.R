db4a160e6a390bf473cfc716526331cf4f5c2ee8
Gene Rogers <grogers@pillartechnology.com>
2016-04-10 18:46:02 -0400

    Added comments

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,12 +1,17 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
+  ##create a character vector of the names of files in the named directory.
   files <- list.files(directory ,full.names = TRUE) 
   
+  ##create data frame to load data into
   dat <- data.frame()
+  
+  ##create a 'for loop' to enter data into the data frame
   
   for (i in id) {
     dat <- rbind(dat, read.csv(files[i]))
   }
   
+  ## calulate the mean without the NAs of the pollutant
   mean(dat[, pollutant], na.rm = TRUE)
 }
